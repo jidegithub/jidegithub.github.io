26780f66b66aa65c65b6f19f61abf4ecd92ae8b1
olajide.o <olajide.o@ng.lopworks.com>
2020-07-02 21:19:01 +0100

    styled 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,22 +2,40 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
+import fourOfour from "../images/404.svg";
+import rocket from "../images/rocket.svg";
+import earth from "../images/earth.svg";
+import moon from "../images/moon.svg";
+import astronaut from "../images/astronaut.svg"
 
 function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div>
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={abductionIllustration}
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
-        </h2>
-      </div>
+      <section className="mb-20">
+        <div className="stars">
+          <div className="flex items-center justify-center">
+            <img className="image-404" src={fourOfour} width="300px"/>
+          </div>
+          <div className="objects">
+            <img className="object_rocket" src={rocket} width="40px"/>
+              <div className="earth-moon">
+                <img className="object_earth" src={earth} width="100px"/>
+                <img className="object_moon" src={moon} width="80px"/>
+              </div>
+              <div className="box_astronaut">
+                <img className="object_astronaut" src={astronaut} width="140px"/>
+              </div>
+          </div>
+          <div className="glowing_stars">
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
