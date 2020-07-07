8c08e4b3db2bc2a3f8606e1e4d58a2580f86efbd
olajide.o <olajide.o@ng.lopworks.com>
2020-07-08 00:27:51 +0100

    modified header mobile view for easy a11y

--- src/components/header.js
+++ src/components/header.js
@@ -15,31 +15,17 @@ function Header() {
 
   return (
     <header className="">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-4 md:p-8">
-        <Link className="flex items-center no-underline text-white" to="/">
+      <div className="flex flex-wrap justify-between mx-auto smx-pd-40 sm:p-8 p-4 md:p-8">
+        <Link className="flex smx-mt-15 lg:mt-0 md:mt-0 sm:mt-4 no-underline text-white" to="/">
           <span className="font-bold text-xl tracking-tight">
             {site.siteMetadata.title}
           </span>
         </Link>
 
-        {/* <button
-          className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
-          onClick={() => toggleExpansion(!isExpanded)}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button> */}
-
         <nav
           className={`${
-            isExpanded ? `block` : `hidden`
-            } md:block md:flex md:items-center w-full md:w-auto`}
+            isExpanded ? `block` : ``
+            } md:block md:flex md:w-auto`}
         >
           {[
             {
@@ -63,7 +49,7 @@ function Header() {
               className="block sf-mono-font text-sm name-grey anti-main-color-hover md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-white"
               key={link.title}
               to={link.route}
-              activeClassName={`border-b-2`}
+              // activeClassName={`border-b-2`}
             >
               {link.title}
             </Link>
