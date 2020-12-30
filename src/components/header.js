e16b096351a24fe71b3bc971060d16a720e83d64
olajide.o <olajide.o@ng.lopworks.com>
2020-12-30 17:47:50 +0100

    update:update header margin on diff breakpoints

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,7 @@ function Header() {
 
   return (
     <header className="w-full fixed z-50">
-      <div className="flex justify-between smx-pd-40 sm:p-8 smx-flex-col p-4 md:p-8 mx-20">
+      <div className="flex justify-between smx-pd-40 sm:p-8 smx-flex-col p-4 md:p-8 lg:mx-20 md:mx-20">
         <Link className="flex smx-mt-15 smx-mb-4 lg:mt-0 md:mt-0 sm:mt-4 no-underline text-white" to="/">
           <span className="font-bold text-xl tracking-tight">
             {site.siteMetadata.title}
